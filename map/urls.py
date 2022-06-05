5816607795b1377a3f2777520b6c257a88247fb2
Alexander Waya Nguku <alex.waya@gmail.com>
2022-06-05 14:59:21 +0300

    app path from re_path

--- map/urls.py
+++ map/urls.py
@@ -1,10 +1,23 @@
-from django.urls import re_path
+# from django.urls import re_path
+
+# from .views import AddPlaceView, ChangePlaceView, PlacesView
+
+# urlpatterns = [
+#     re_path("^$", AddPlaceView.as_view(), name="add"),
+#     re_path("^places/(?P<pk>[0-9]+)/$", ChangePlaceView.as_view(), name="change"),
+#     re_path("^index/$", PlacesView.as_view(), name="index"),
+
+# ]
+
+
+from django.urls import path
 
 from .views import AddPlaceView, ChangePlaceView, PlacesView
 
 urlpatterns = [
-    re_path("^$", AddPlaceView.as_view(), name="add"),
-    re_path("^places/(?P<pk>[0-9]+)/$", ChangePlaceView.as_view(), name="change"),
-    re_path("^index/$", PlacesView.as_view(), name="index"),
+    #re_path("^$", AddPlaceView.as_view(), name="add"),
+    path("", AddPlaceView.as_view(), name="add"),
+    path("places/(?P<pk>[0-9]+)/", ChangePlaceView.as_view(), name="change"),
+    path("index/", PlacesView.as_view(), name="index"),
 
 ]
